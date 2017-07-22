e997dbddef109ca771dbc781d501a97d9caea9cd
seanwestfall <sean.westfall@gmail.com>
2017-07-22 00:05:40 -0700

    Added dropdown

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,6 +1,15 @@
 new Vue({
   el: '#app',
   data: {
-    select = ["one","two","three","four"]
+    select: ["one","two","three","four"]
   }
 })
+
+document.getElementById('dropdown-toggle').addEventListener("click", function( event ) {
+    var dropDownDisplay = document.getElementById('dropdown-menu').style.display;
+    if(dropDownDisplay == "block") {
+    	dropDownDisplay = "none";
+    } else {
+    	dropDownDisplay = "block";
+    }
+});
